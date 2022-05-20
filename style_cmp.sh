1988391d8fe09856ba65049fb8561729aa27daee
schmilly <schmilly@localhost.localdomain>
2022-05-20 20:26:12 +0800

    Fixed up some errors made on mobile, is now functional (unlike before) :)

--- style_cmp.sh
+++ style_cmp.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 #checking to see that input does exit, if not print error message
-
+debug=0
 #usage text, output when needed.
-Usage = "Usage: style_cmp.sh [ <directory of file to create profile off of> | <directory of file to analyze> <directory of profile> ]"
+Usage="Usage: style_cmp.sh [ <directory of file to create profile off of> | <directory of file to analyze> <directory of profile> ]"
 
 #toggles based on existancr of second paramters
-profilemode = 1
+profilemode=1
 
 #checks of inouts to ensure existance of inputs
 if [[ -z "$1" ]]; then
-  1>&2 echo "Please specify an argument, non detected"
+  1>&2 echo "Please specify an file location, none detected"
   echo $Usage
   exit
 elif [[ ! -f $1 ]]
@@ -19,12 +19,15 @@ then
   echo $Usage
   exit 1
 elif [[ -z "$2" ]]; then
-  $profilemode = 0
   if [[ ! -f $2 ]]; then
     1>&2 echo "Could not find specified file $2"
     echo $Usage
     exit 1
   fi
+  profilemode = 1
+  echo "Two specified files detected, entering analysis mode..."
+else
+  echo "Only One specified file detected, entering profile genration mode..."
 fi
 
 #array of words to use, can be changed (obvi)
