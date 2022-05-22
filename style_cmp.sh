cf0c0af8adfae69a2097a01052704ab98be56ecd
schmilly <schmilly@localhost.localdomain>
2022-05-23 00:50:02 +0800

    Added some more lines and stuff, need to be done by tommorwo. I sleep now :)

--- style_cmp.sh
+++ style_cmp.sh
@@ -4,31 +4,38 @@
 debug=0
 #usage text, output when needed.
 Usage="Usage: style_cmp.sh [ <directory of file to create profile off of> | <directory of file to analyze> <directory of profile> ]"
+#Specified words for the profile to look for
+Wordarray=("also" "although" "and" "as" "because" "before" "but" "for" "if" "nor" "of" "or" "since" "that" "though" "until" "when" "whenever" "whereas" "which" "while" "yet")
 
 #toggles based on existancr of second paramters
 profilemode=1
 
 #checks of inouts to ensure existance of inputs
-if [[ -z "$1" ]]; then
+if [[ -z $1 ]]; then
   1>&2 echo "Please specify an file location, none detected"
   echo $Usage
   exit
-elif [[ ! -f $1 ]]
-then
-  1>%2 echo "Could not find specified file $1"
+elif [[ ! -f $1 ]]; then
+  1>&2 echo "Could not find specified file $1"
   echo $Usage
   exit 1
-elif [[ -z "$2" ]]; then
+elif [[ ! -z $2 ]]; then
   if [[ ! -f $2 ]]; then
     1>&2 echo "Could not find specified file $2"
     echo $Usage
     exit 1
   fi
-  profilemode = 1
+  profilemode=0
   echo "Two specified files detected, entering analysis mode..."
 else
   echo "Only One specified file detected, entering profile genration mode..."
 fi
 
-#array of words to use, can be changed (obvi)
-wordarray = ("also" "although" "and" "as" "because" "before" "but" "for" "if" "nor" "of" "or" "since" "that" "though" "until" "when" "whenever" "whereas" "which" "while" "yet")
+for i in "${Wordarray[@]}"
+do
+  if [ $profilemode == 1 ]; then
+  echo "Profilemode detected"
+  echo $i
+  fi
+done
+
